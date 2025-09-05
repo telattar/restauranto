44a5446a6f4f1dad462e8ee5f3ce0149e179a0e4
telattar <tarteelafattahibrahim@gmail.com>
2025-09-05 13:14:05 +0300

    fix: update semantic release git configs

--- release.config.mjs
+++ release.config.mjs
@@ -7,7 +7,14 @@ export default {
     '@semantic-release/commit-analyzer', // figure out bump type (major/minor/patch)
     '@semantic-release/release-notes-generator', // generate changelog text
     ['@semantic-release/changelog', { changelogFile: 'CHANGELOG.md' }],
-    ['@semantic-release/git', { assets: ['CHANGELOG.md', 'package.json'] }],
+    [
+      '@semantic-release/git',
+      {
+        assets: ['package.json'], // only bump version in package.json
+        message: 'chore(release): bump version to ${nextRelease.version}',
+        author: 'tarteelafattahibrahim@gmail.com',
+      },
+    ],
     '@semantic-release/github',
   ],
 };
